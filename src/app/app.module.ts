deed01867f774d4f4537d0661f115c9e777028a7
nk1aus <67040497+nk1aus@users.noreply.github.com>
2022-05-04 14:58:26 +0300

    added cart modal, edited itemPage
    Not finished
    animation not working
    a lot of redundent codes that  needs to be removed or fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CartModalPageModule } from './cart-modal/cart-modal.module';
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, CartModalPageModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
