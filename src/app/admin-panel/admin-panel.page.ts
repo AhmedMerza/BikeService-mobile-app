3b46d2e615845bac99f4d455c44b75807794d707
vnxkaito <81317627+vnxkaito@users.noreply.github.com>
2022-05-16 23:57:56 +0300

    Update admin-panel.page.ts

--- src/app/admin-panel/admin-panel.page.ts
+++ src/app/admin-panel/admin-panel.page.ts
@@ -37,6 +37,116 @@ export class AdminPanelPage implements OnInit {
   public options3: Partial<ChartOptions>;
   public options4: Partial<ChartOptions>;
 
+  public totalOrders: number;
+  public totalAmount: number;
+
+  // each index represent a day, iterate through order and add 1 to the value of the corresponding index
+  // The array must be defined with 0 values and the length must be based on number of days in current month
+  public dayOfCurrentMonth: number;
+  public ordersInMonth: number[];
+
+  // each index represent a month, iterate through order and add 1 to the value of the corresponding index
+  public ordersInYear: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  //heatmap
+  public ordersByTimeAndDay: [{
+    name: '12AM',
+    data: [0, 0, 0, 0, 0, 0, 0]
+  },
+    {
+      name: '1AM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '2AM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '3AM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '4AM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '5AM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '6AM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '7AM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '8AM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '9AM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '10AM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '11AM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '12PM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '1PM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '2PM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '3PM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '4PM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '5PM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '6PM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '7PM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '8PM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '9PM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '10PM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+      name: '11PM',
+      data: [0, 0, 0, 0, 0, 0, 0]
+    }
+  ];
+
   constructor() {
     this.spackLine()
   }
@@ -75,7 +185,7 @@ export class AdminPanelPage implements OnInit {
         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
       }
     };
-    
+
     this.options3 = {
       series: [{
         name: '12AM',
@@ -209,7 +319,7 @@ export class AdminPanelPage implements OnInit {
       title: {
         text: "Categories analysis"
       },
-      
+
 
     };
 
@@ -248,9 +358,52 @@ export class AdminPanelPage implements OnInit {
 
   };
 
-
-
   ngOnInit() {
+    const d = new Date();
+    var month = d.getMonth() + 1;	// Month	[mm]	(1 - 12)
+    var day = d.getDate();		// Day		[dd]	(1 - 31)
+    var year = d.getFullYear();	// Year		[yyyy]
+
+    switch (month) {
+      case 1:
+        this.dayOfCurrentMonth = 31;
+        break;
+      case 2:
+        this.dayOfCurrentMonth = 28;
+        break;
+      case 3:
+        this.dayOfCurrentMonth = 31;
+        break;
+      case 4:
+        this.dayOfCurrentMonth = 30;
+        break;
+      case 5:
+        this.dayOfCurrentMonth = 31;
+        break;
+      case 6:
+        this.dayOfCurrentMonth = 30;
+        break;
+      case 7:
+        this.dayOfCurrentMonth = 31;
+        break;
+      case 8:
+        this.dayOfCurrentMonth = 31;
+        break;
+      case 9:
+        this.dayOfCurrentMonth = 30;
+        break;
+      case 10:
+        this.dayOfCurrentMonth = 31;
+        break;
+      case 11:
+        this.dayOfCurrentMonth = 30;
+        break;
+      case 12:
+        this.dayOfCurrentMonth = 31;
+        break;
+
+    }
+
   }
 
 }
